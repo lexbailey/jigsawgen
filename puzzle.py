e006c425bf3a25186fcfead7ffa7f066c569a8e9
Daniel Bailey <danieljabailey@gmail.com>
2017-08-11 22:40:56 +0100

    make edges smooth and make edges look like corners

--- puzzle.py
+++ puzzle.py
@@ -22,6 +22,12 @@ outputname = args.image + ".svg"
 onecellw = args.widthmm / args.cellsw
 onecellh = args.heightmm / args.cellsh
 
+def isodd(num):
+    return num & 1
+
+if isodd(args.cellsw) or isodd(args.cellsh):
+    print("Warning, odd cell numbers make some corners obvious.")
+
 dwg = svgwrite.Drawing(outputname)
 
 stroke = svgwrite.rgb(0,0,0)
@@ -55,7 +61,6 @@ def blob_start(direction, cellcentre, invert=False):
         return cellcentre[0] + offsetw, cellcentre[1] - toedgeh
 
 def connector_nums():
-    #dist = (random.random()/4) + (3/8)
     dist = (random.random()/6) + (5/12)
     width = random.random()/8 + (1/6)
     return dist, width
@@ -68,28 +73,30 @@ def partial_line(start, end, dist, onto=None):
         onto = start
     return tuple((o + ((e-s)*dist)) for o, s, e in zip(onto, start, end))
 
-def edge_line(dwg, centre, startpoint, endpoint, blobdir):
+def edge_line(dwg, centre, startpoint, endpoint, blobdir, flat=False):
     path = dwg.path(stroke=stroke, fill="none")
     path.push("M", *startpoint)
-    dist, width = connector_nums()
-    connector_start = dist - (width/2)
-    connector_end = dist + (width/2)
-    path.push("L", *partial_line(startpoint, endpoint, connector_start))
+    if not flat:
+        dist, width = connector_nums()
+        connector_start = dist - (width/2)
+        connector_end = dist + (width/2)
+        path.push("L", *partial_line(startpoint, endpoint, connector_start))
 
-    inv = random.choice([True, False])
-    blob_s = blob_start(blobdir, centre, invert=inv)
+        inv = random.choice([True, False])
+        blob_s = blob_start(blobdir, centre, invert=inv)
 
-    conn_1 = partial_line(startpoint, endpoint, connector_start-width/2, onto=blob_s)
-    conn_2 = partial_line(startpoint, endpoint, connector_end+width/2, onto=blob_s)
-    path.push("L", *conn_1)
-    path.push("L", *conn_2)
+        conn_1 = partial_line(startpoint, endpoint, connector_start-width/2, onto=blob_s)
+        conn_2 = partial_line(startpoint, endpoint, connector_end+width/2, onto=blob_s)
+        path.push("L", *conn_1)
+        path.push("L", *conn_2)
     
-    path.push("L", *partial_line(startpoint, endpoint, connector_end))
+        path.push("L", *partial_line(startpoint, endpoint, connector_end))
+
     path.push("L", *endpoint)
     dwg.add(path)
 
 for x, y in itertools.product(range(args.cellsw), range(args.cellsh)):
-    print("%d, %d" % (x,y))
+    #print("%d, %d" % (x,y))
     xmm = x * onecellw
     ymm = y * onecellh
 
@@ -101,10 +108,13 @@ for x, y in itertools.product(range(args.cellsw), range(args.cellsh)):
     centre = cellcentre(x, y)
 
     if x == 0:
-        edge_line(dwg, centre, topleft, bottomleft, 'left')
+        edge_line(dwg, centre, topleft, bottomleft, 'left', flat=True)
     if y == 0:
-        edge_line(dwg, centre, topleft, topright, 'up')
-    edge_line(dwg, centre, topright, bottomright, 'right')
-    edge_line(dwg, centre, bottomleft, bottomright, 'down')
+        edge_line(dwg, centre, topleft, topright, 'up', flat=True)
+
+    flat_right = x==args.cellsw-1 or (y in[0,args.cellsh-1] and x%2==1)
+    flat_bottom = y==args.cellsh-1 or (x in[0, args.cellsw-1] and y%2==1)
+    edge_line(dwg, centre, topright, bottomright, 'right', flat=flat_right)
+    edge_line(dwg, centre, bottomleft, bottomright, 'down', flat=flat_bottom)
 
 dwg.save()
